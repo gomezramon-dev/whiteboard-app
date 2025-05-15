e7d7eeabdeec35e65392e2fa39c97feca5e7625e
gomezramon-dev <gomez.ramon2004@gmail.com>
2025-05-15 13:13:52 -0700

    feat: disable drawing when resizing

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,46 @@
+import { useState, useEffect, useRef } from "react";
 import CanvasComponent from "@components/Canvas";
 
 const App = () => {
+  const [isWindowResizing, setIsWindowsResizing] = useState(false);
+  const timeOutFunctionId = useRef<NodeJS.Timeout | null>(null);
+
+  /**
+   * * Manejo del redimensionamiento de la ventana:
+   * * - Define la función handleResize que:
+   * *     - Limpia cualquier setTimeout previo para evitar múltiples ejecuciones pendientes.
+   * *     - Establece el estado isWindowResizing a true.
+   * *     - Inicia un nuevo setTimeout para regresar isWindowResizing a false después de 500ms.
+   * * - Al montar el componente:
+   * *     - Agrega un event listener al evento 'resize' del objeto window que llama a handleResize.
+   * * - Al desmontar el componente:
+   * *     - Remueve el event listener de 'resize' de window.
+   * *     - Limpia cualquier timeout pendiente al desmontar para evitar efectos secundarios fuera del ciclo de vida.
+   */
+  const handleResize = () => {
+    if (timeOutFunctionId.current) clearTimeout(timeOutFunctionId.current);
+
+    setIsWindowsResizing(true);
+    timeOutFunctionId.current = setTimeout(
+      () => setIsWindowsResizing(false),
+      100,
+    );
+  };
+
+  useEffect(() => {
+    window.addEventListener("resize", handleResize);
+    return () => {
+      window.removeEventListener("resize", handleResize);
+      if (timeOutFunctionId.current) clearTimeout(timeOutFunctionId.current);
+    };
+  }, []);
+
   return (
     <div id="app" className="flex flex-col p-12 min-h-screen">
       <div className="bg-white-color border-4 border-black-color rounded-2xl shadow-block mb-8 h-32">
         Colors & Tools
       </div>
-      <CanvasComponent />
+      <CanvasComponent isWindowResizing={isWindowResizing} />
     </div>
   );
 };
--- src/components/Canvas.tsx
+++ src/components/Canvas.tsx
@@ -1,36 +1,77 @@
 import { useState, useEffect, useRef } from "react";
 
-interface CanvasSizeState {
-  cssWidth: number;
-  cssHeight: number;
+interface CanvasBufferSizeState {
+  width: number;
+  height: number;
 }
 
-const CanvasComponent: React.FC = () => {
-  const canvasRef = useRef<HTMLCanvasElement | null>(null);
-  const [size, setSize] = useState<CanvasSizeState>({
-    cssWidth: 0,
-    cssHeight: 0,
-  });
-  const sizeRef = useRef(size);
+interface CanvasProps {
+  isWindowResizing: boolean;
+}
 
-  useEffect(() => {
-    sizeRef.current = size;
-  }, [size]);
+const CanvasComponent: React.FC<CanvasProps> = ({ isWindowResizing }) => {
+  const canvasRef = useRef<HTMLCanvasElement | null>(null);
+  const [devicePixelRatio, setDevicePixelRatio] = useState(
+    typeof window !== "undefined" ? window.devicePixelRatio || 1 : 1,
+  );
+  const [bufferSize, setBufferSize] = useState<CanvasBufferSizeState>({
+    width: 0,
+    height: 0,
+  });
+
+  /**
+   * * Redimensionar:
+   * * - Toma referencia del elemento Canvas del DOM
+   * *    - Si no existe referencia, se termina el useEffect
+   * * - Crea un objeto ResizeObserver, que por cada entrada actualiza el tamaño del CSS
+   * *    - Se calcula un nuevo valor de densidad de pixeles.
+   * *    - Se calcula el nuevo tamaño del canvas; producto del tamaño de la entry con la densidad de pixeles.
+   * *    - Se actualiza la densidad de pixeles.
+   * *        - Si el estado anterior es el mismo que el actual, se regresa el anterior
+   * *    - Se actualiza el tamaño del canvas
+   * *        - Si el estado anterior es el mismo que el actual, se regresa el anterior
+   * * - Tal objeto se le relaciona con la referencia del Canvas
+   * * - El ResizeObserver se deslinda del Canvas una vez se haya desmontado el componente
+   */
+  const resizeHandler: ResizeObserverCallback = (entries) => {
+    for (const entry of entries) {
+      const newDevicePixelRatio = window.devicePixelRatio | 1;
+
+      const calculatedBufferWidth = Math.floor(
+        entry.contentRect.width * newDevicePixelRatio,
+      );
+      const calculatedBufferHeight = Math.floor(
+        entry.contentRect.height * newDevicePixelRatio,
+      );
+
+      setDevicePixelRatio((prevDevicePixelRatio) => {
+        if (prevDevicePixelRatio !== newDevicePixelRatio)
+          return newDevicePixelRatio;
+
+        return prevDevicePixelRatio;
+      });
+
+      setBufferSize((prevBufferSize) => {
+        if (
+          prevBufferSize.width !== calculatedBufferWidth ||
+          prevBufferSize.height !== calculatedBufferHeight
+        ) {
+          return {
+            width: calculatedBufferWidth,
+            height: calculatedBufferHeight,
+          };
+        }
+
+        return prevBufferSize;
+      });
+    }
+  };
 
   useEffect(() => {
     const canvasElementForResize = canvasRef.current;
-    if (!canvasElementForResize) {
-      return;
-    }
+    if (!canvasElementForResize) return;
 
-    const resizeObserver = new ResizeObserver((entries) => {
-      for (const entry of entries) {
-        setSize({
-          cssWidth: entry.contentRect.width,
-          cssHeight: entry.contentRect.height,
-        });
-      }
-    });
+    const resizeObserver = new ResizeObserver(resizeHandler);
 
     resizeObserver.observe(canvasElementForResize);
 
@@ -39,41 +80,43 @@ const CanvasComponent: React.FC = () => {
     };
   }, []);
 
-  const currentDevicePixelRatio =
-    typeof window !== "undefined" ? window.devicePixelRatio || 1 : 1;
-  const bufferWidth = Math.floor(size.cssWidth * currentDevicePixelRatio);
-  const bufferHeight = Math.floor(size.cssHeight * currentDevicePixelRatio);
-
+  /**
+   * * Dibuja:
+   * * - Toma referencia del elemento Canvas del DOM
+   * *    - Si no existe referencia, se termina el useEffect
+   * * - Obtiene el contexto 2D del canvas para empezar a dibujar
+   * *    - Si no existe contexto, se termina el useEffect
+   * * - Si no existe largo ni alto en el canvas, o la pantalla se está renderizando se termina el useEffect.
+   * *   No vale la pena renderizar algo que no existe.
+   * * - Empieza a dibujar.
+   */
   useEffect(() => {
-    if (bufferWidth === 0 || bufferHeight === 0) {
-      return;
-    }
     const canvasElementForDraw = canvasRef.current;
-    if (!canvasElementForDraw) {
-      return;
-    }
+    if (!canvasElementForDraw) return;
 
     const ctx = canvasElementForDraw.getContext("2d");
-    if (!ctx) {
+    if (!ctx) return;
+
+    if (isWindowResizing || bufferSize.width === 0 || bufferSize.height === 0) {
+      ctx.clearRect(0, 0, bufferSize.width, bufferSize.height);
       return;
     }
 
     ctx.resetTransform?.();
-    ctx.clearRect(0, 0, bufferWidth, bufferHeight);
-    ctx.scale(currentDevicePixelRatio, currentDevicePixelRatio);
-
+    ctx.clearRect(0, 0, bufferSize.width, bufferSize.height);
+    ctx.scale(devicePixelRatio, devicePixelRatio);
     ctx.fillStyle = "#000";
     ctx.beginPath();
-    ctx.arc(50, 100, 20, 0, 2 * Math.PI); // Coordenadas en "píxeles CSS"
+    ctx.arc(50, 100, 20, 0, 2 * Math.PI);
     ctx.fill();
-  }, [bufferWidth, bufferHeight, currentDevicePixelRatio]);
+  }, [isWindowResizing, bufferSize, devicePixelRatio]);
 
   return (
     <canvas
       className="bg-white-color border-4 border-black-color rounded-2xl shadow-block flex-1 w-full"
       ref={canvasRef}
-      width={bufferWidth}
-      height={bufferHeight}
+      width={bufferSize.width}
+      height={bufferSize.height}
     />
   );
 };
